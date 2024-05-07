d80f4b8da2216073a76b751822266a31972b850f
mr-maksim <55948966+mr-maksim@users.noreply.github.com>
2024-05-07 04:04:56 +0300

    Added function "new_polices"

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import sys
+from PyQt6 import QtWidgets
 from PySide6.QtWidgets import QApplication,QMainWindow
 
 from ui.home import Ui_MainWindow
-
+from ui.edit import Ui_Dialog
+from db_connect import Data
 
 class CTP(QMainWindow):
     def __init__(self) -> None:
         super(CTP,self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.connect = Data()
+
+    def new_polices(self):
+        self.new_window = QtWidgets.QDialog()
+        self.ui_window.setupui(self.new_window)
+        self.new_window.show()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
